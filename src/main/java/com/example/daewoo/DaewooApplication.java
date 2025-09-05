3b96ae6d64fb22766193a57dae4a0741f75f33b7
Se0jwon <sjso11426682@gmail.com>
2025-09-05 19:19:33 +0900

    first commit

---
+++ src/main/java/com/example/daewoo/DaewooApplication.java
@@ -0,0 +1,13 @@
+package com.example.daewoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaewooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaewooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daewoo/DaewooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daewoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaewooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
