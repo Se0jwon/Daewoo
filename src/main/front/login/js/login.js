7019026cc10767b010f940e969c24dc531f1f884
seo-04 <coups1234@naver.com>
2025-09-07 14:24:06 +0900

    로그인 스크립트 수정
    수정

--- src/main/front/login/js/login.js
+++ src/main/front/login/js/login.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleIcon = document.querySelector(".passwordbox i");
     const passwordInput = document.querySelector(".passwordbox input[type='password']");
+    const loginForm = document.getElementById("loginForm");
+    const emailInput = loginForm.querySelector("input[name='userName']");
 
+    // 비밀번호 토글 기능
     toggleIcon.addEventListener("click", function () {
         if (passwordInput.type === "password") {
             passwordInput.type = "text";
@@ -11,6 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
             passwordInput.type = "password";
             toggleIcon.classList.remove("fa-eye");
             toggleIcon.classList.add("fa-eye-slash");
+        }
+    });
+
+    // 입력값 체크
+    loginForm.addEventListener("submit", function (e) {
+        if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+            e.preventDefault(); // 폼 제출 막기
+            alert("이메일과 비밀번호를 모두 입력해주세요!");
         }
     });
 });
