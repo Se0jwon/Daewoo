b84f9321cccca89edcfc2e6a582c5820018910d9
Se0jwon <sjso11426682@gmail.com>
2025-09-08 20:24:31 +0900

    숙소, 유저 crud api생성 reload

--- src/main/java/com/example/daewoo/DaewooApplication.java
+++ src/main/java/com/example/daewoo/DaewooApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.daewoo.user")
+@MapperScan("com.example.daewoo")
 public class DaewooApplication {
 
     public static void main(String[] args) {
