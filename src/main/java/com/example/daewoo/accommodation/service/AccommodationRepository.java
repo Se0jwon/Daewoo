db4217c62552ef2775294567d54dabc72db926d5
Se0jwon <sjso11426682@gmail.com>
2025-09-11 20:19:57 +0900

    USER

--- src/main/java/com/example/daewoo/accommodation/service/AccommodationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.daewoo.accommodation.service;
-
-import com.example.daewoo.accommodation.dto.AccommodationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccommodationRepository extends JpaRepository<AccommodationEntity, Long> {
-}
