f17cdd8d8a44ae3137ec2ca3b3a9283742e4e640
Lee-HaeSung <haesung116@naver.com>
2025-09-12 18:50:15 +0900

    9.12) 카드 클릭 및 카드 추가창 기능

--- src/main/fronted/mypage/add_card.js
+++ src/main/fronted/mypage/add_card.js
@@ -1,45 +1,18 @@
-// // 모달을 열고 닫는 함수
-// function openCardModal() {
-//     const modal = document.getElementById('card-add-modal');
-//     modal.style.display = 'block'; // 모달을 보이게 함
-// }
-//
-// function closeCardModal() {
-//     const modal = document.getElementById('card-add-modal');
-//     modal.style.display = 'none'; // 모달을 숨김
-// }
-//
-// // 카드를 추가하는 함수
-// function addNewCard() {
-//     const existingCardContainer = document.querySelector('.existing-card');
-//
-//     const cardInfo = {
-//         last4Digits:'2232',
-//         expiry: '02/27',
-//         cvc:'626',
-//         name_on_card:'LEE JongSU',
-//         country_or_region: 'united states'
-//     };
-//
-//     const newCardElement = createCardElement(cardInfo);
-//
-//     existingCardContainer.appendChild(newCardElement);
-//     closeCardModal(); // 카드 추가 후 모달을 닫음
-// }
-//
-// // 카드 HTML 요소를 생성하는 함수 (수정 사항 없음)
-// function createCardElement(cardInfo){
-//     const cardDiv = document.createElement('div');
-//     cardDiv.classList.add('registered-card');
-//
-//     cardDiv.innerHTML = `
-//     <div class="card-number"> **** **** **** ${cardInfo.last4Digits}</div>
-//     <div class="card-details">
-//         <span>Valid thru</span>
-//         <span class="expiry-date">${cardInfo.expiry}</span>
-//         <span class="cvc-numer">${cardInfo.cvc}</span>
-//     </div>
-//     `;
-//
-//     return cardDiv;
-// }
+//HTML에서 모달과 버튼 요소 가져오기
+const addCardBtn = document.querySelector('.add-card');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-button');
+
+addCardBtn.addEventListener('click', function (){
+    modalOverlay.classList.add('show');
+});
+
+closeBtn.addEventListener('click', function (){
+    modalOverlay.classList.remove('show');
+});
+
+modalOverlay.addEventListener('click', function(event){
+    if(event.target === modalOverlay){
+        modalOverlay.classList.remove('show');
+    }
+});
